25d7f922a6a84b2b5d121d84396e24d978001cff
Nour sabouny <n.sabouny@gmail.com>
2022-08-24 01:28:07 +0400

    update shipment controller basic tests

--- src/WebApi/Controllers/ShipmentsController.cs
+++ src/WebApi/Controllers/ShipmentsController.cs
@@ -1,9 +1,18 @@
+using MediatR;
+
 namespace WebApi;
 
 [ApiController]
 [Route("[controller]")]
 public class ShipmentsController : Controller
 {
+    private IMediator _mediator;
+
+    public ShipmentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     [HttpPost("deliver")]
     public IActionResult Deliver([FromBody] Delivery request)
     {
--- test/WebApi.Tests/Controllers/ShipmentControllerTests.cs
+++ test/WebApi.Tests/Controllers/ShipmentControllerTests.cs
@@ -1,18 +1,32 @@
 
 
+using MediatR;
+using Moq;
+
 namespace WebApi.Tests.Controllers;
 
 public class ShipmentsControllerTests
 {
+    /// <summary>
+    /// System under test (controller)
+    /// </summary>
+    private readonly ShipmentsController _sut;
+    private readonly Mock<IMediator> _mediator;
+
+    public ShipmentsControllerTests()
+    {
+        _mediator = new Mock<IMediator>();
+        _sut = new ShipmentsController(_mediator.Object);
+    }
+
     [Fact]
     public void Deliver_Returns_OkResult_When_Valid_Request_Recieved()
     {
         // Arrange
-        var controller = new ShipmentsController();
         var fakeRequest = new AutoFaker<Delivery>().Generate();
 
         // Act
-        var result = controller.Deliver(fakeRequest);
+        var result = _sut.Deliver(fakeRequest);
 
         // Assert
         Assert.IsType<OkResult>(result);
