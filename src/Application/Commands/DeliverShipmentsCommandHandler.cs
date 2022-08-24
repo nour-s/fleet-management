f915f7f033679f4b001aa3d9d623a8713d8b22fd
Nour sabouny <n.sabouny@gmail.com>
2022-08-25 00:57:02 +0400

    inject repositoris in command handler

--- src/Application/Commands/DeliverShipmentsCommandHandler.cs
+++ src/Application/Commands/DeliverShipmentsCommandHandler.cs
@@ -1,9 +1,21 @@
+using Application.Persistence;
+using Domain.Models;
 using MediatR;
 
 namespace Application.Commands;
 
 public class DeliverShipmentsCommandHandler : IRequestHandler<DeliverShipmentsCommand>
 {
+    private readonly IRepository<Sack> _sackRepository;
+
+    private readonly IRepository<Package> _packageRepository;
+
+    public DeliverShipmentsCommandHandler(IRepository<Sack> sackRepository, IRepository<Package> packageRepository)
+    {
+        _sackRepository = sackRepository;
+        _packageRepository = packageRepository;
+    }
+
     public Task<Unit> Handle(DeliverShipmentsCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
