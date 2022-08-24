056a00c2cd5f1c2ec9b5e16c5d18b30f4ead8c1b
Nour sabouny <n.sabouny@gmail.com>
2022-08-25 00:57:32 +0400

    update unit tests to consider the repo pattern

--- test/Application.Tests/DeliverShipmentsCommandHandlerTests.cs
+++ test/Application.Tests/DeliverShipmentsCommandHandlerTests.cs
@@ -1,5 +1,8 @@
 using Application.Commands;
+using Application.Persistence;
+using Domain.Models;
 using WebApi.Models;
+using Moq;
 
 namespace Application.Tests;
 
@@ -7,9 +10,14 @@ public class DeliverShipmentsCommandHandlerTests
 {
     private readonly DeliverShipmentsCommandHandler _sut;
 
+    // mock sack repo
+    private readonly Mock<IRepository<Sack>> _sackRepositoryMock = new();
+    private readonly Mock<IRepository<Package>> _packageRepositoryMock = new();
+
+
     public DeliverShipmentsCommandHandlerTests()
     {
-        _sut = new DeliverShipmentsCommandHandler();
+        _sut = new DeliverShipmentsCommandHandler(_sackRepositoryMock.Object, _packageRepositoryMock.Object);
     }
 
     [Fact]
