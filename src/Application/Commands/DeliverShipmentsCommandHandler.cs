1e366f8cce93ee3b13f641010282f0b1dd6e4c59
Nour sabouny <n.sabouny@gmail.com>
2022-08-25 22:36:18 +0400

    define a commandhandler for DeliverShipment

--- src/Application/Commands/DeliverShipmentsCommandHandler.cs
+++ src/Application/Commands/DeliverShipmentsCommandHandler.cs
@@ -16,8 +16,58 @@ public class DeliverShipmentsCommandHandler : IRequestHandler<DeliverShipmentsCo
         _packageRepository = packageRepository;
     }
 
-    public Task<Unit> Handle(DeliverShipmentsCommand request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(DeliverShipmentsCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        foreach (var route in request.Delivery.Routes)
+        {
+            var deliveryPoint = (DeliveryPointType)route.DeliveryPoint;
+
+            foreach (var shipment in route.Deliveries)
+            {
+                var isItPackage = shipment.Barcode.StartsWith("P");
+
+                if (isItPackage)
+                {
+                    var package = await _packageRepository.SingleOrDefaultAsync(x => x.Barcode == shipment.Barcode, x => x.Sack!);
+
+                    if (package == null)
+                        continue;
+
+                    package.Load();
+
+                    try
+                    {
+                        package.Unload(deliveryPoint);
+                    }
+                    catch (ArgumentException)
+                    {
+                        // package can't be unloaded to this delivery point, we just skip.
+                        continue;
+                    }
+                }
+                else
+                {
+                    var sack = await _sackRepository.SingleOrDefaultAsync(x => x.Barcode == shipment.Barcode, x => x.Packages);
+                    if (sack == null)
+                        continue;
+
+                    sack.Load();
+
+                    try
+                    {
+                        sack.Unload(deliveryPoint);
+                    }
+                    catch (ArgumentException)
+                    {
+                        // sack can't be unloaded to this delivery point, we just skip.
+                        continue;
+                    }
+                }
+            }
+        }
+
+        await _packageRepository.SaveChangesAsync();
+        await _sackRepository.SaveChangesAsync();
+        return Unit.Value;
     }
 }
