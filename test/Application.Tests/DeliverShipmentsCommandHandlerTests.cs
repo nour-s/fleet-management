d21615fea96827b917fc23f4fdf12313b8f2775a
Nour sabouny <n.sabouny@gmail.com>
2022-08-25 22:37:23 +0400

    add unit tests for the command handler

--- test/Application.Tests/DeliverShipmentsCommandHandlerTests.cs
+++ test/Application.Tests/DeliverShipmentsCommandHandlerTests.cs
@@ -3,6 +3,8 @@ using Application.Persistence;
 using Domain.Models;
 using WebApi.Models;
 using Moq;
+using System.Linq.Expressions;
+using Bogus;
 
 namespace Application.Tests;
 
@@ -14,17 +16,94 @@ public class DeliverShipmentsCommandHandlerTests
     private readonly Mock<IRepository<Sack>> _sackRepositoryMock = new();
     private readonly Mock<IRepository<Package>> _packageRepositoryMock = new();
 
-
     public DeliverShipmentsCommandHandlerTests()
     {
-        _sut = new DeliverShipmentsCommandHandler(_sackRepositoryMock.Object, _packageRepositoryMock.Object);
+        _sut = new DeliverShipmentsCommandHandler(
+            _sackRepositoryMock.Object,
+            _packageRepositoryMock.Object
+        );
     }
 
     [Fact]
-    public void Handle_Should_Query_Database_For_Every_Shipment()
+    public async Task Handle_Should_Process_A_Shipment_Of_Type_Package()
     {
-        var command = new DeliverShipmentsCommand(new Delivery());
+        // Arrange
+        var selectedDeliveryPoint = DeliveryPointType.Branch;
+        var packageBarcode = new Randomizer().Replace("P##########");
 
-        _sut.Handle(command, CancellationToken.None);
+        var package = new Package(packageBarcode, selectedDeliveryPoint, new Random().Next());
+
+        var delivery = new Delivery
+        {
+            Routes = new List<Route>
+            {
+                new Route
+                {
+                    DeliveryPoint = (int)selectedDeliveryPoint,
+                    Deliveries = new List<Shipment> { new Shipment { Barcode = packageBarcode } }
+                }
+            }
+        };
+
+        _packageRepositoryMock
+            .Setup(x => x.SingleOrDefaultAsync(It.IsAny<Expression<Func<Package, bool>>>(), It.IsAny<Expression<Func<Package, object>>>()))
+                .ReturnsAsync(package);
+
+        var command = new DeliverShipmentsCommand(delivery);
+
+        // Act
+
+        await _sut.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        // verify that package is fetched from db.
+        _packageRepositoryMock.Verify(x => x.SingleOrDefaultAsync(It.IsAny<Expression<Func<Package, bool>>>(), It.IsAny<Expression<Func<Package, object>>[]>()), Times.Once);
+        _packageRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+
+        // Assert that the package is unloaded
+        Assert.Equal(PackageState.Unloaded, package.State);
+    }
+
+
+    [Fact]
+    public async Task Handle_Should_Process_A_Shipment_Of_Type_Sack()
+    {
+        // Arrange
+        var selectedDeliveryPoint = DeliveryPointType.Branch;
+        var sackBarcode = new Randomizer().Replace("C##########");
+
+        var sack = new Sack(sackBarcode, selectedDeliveryPoint);
+
+        var delivery = new Delivery
+        {
+            Routes = new List<Route>
+            {
+                new Route
+                {
+                    DeliveryPoint = (int)selectedDeliveryPoint,
+                    Deliveries = new List<Shipment> { new Shipment { Barcode = sackBarcode } }
+                }
+            }
+        };
+
+        _sackRepositoryMock
+            .Setup(x => x.SingleOrDefaultAsync(It.IsAny<Expression<Func<Sack, bool>>>(), It.IsAny<Expression<Func<Sack, object>>>()))
+                .ReturnsAsync(sack);
+
+        var command = new DeliverShipmentsCommand(delivery);
+
+        // Act
+
+        await _sut.Handle(command, CancellationToken.None);
+
+        // Assert
+
+        // verify that package is fetched from db.
+        _sackRepositoryMock.Verify(x => x.SingleOrDefaultAsync(It.IsAny<Expression<Func<Sack, bool>>>(), It.IsAny<Expression<Func<Sack, object>>[]>()), Times.Once);
+        _sackRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+
+        // Assert that the package is unloaded
+        Assert.Equal(SackState.Unloaded, sack.State);
     }
 }
